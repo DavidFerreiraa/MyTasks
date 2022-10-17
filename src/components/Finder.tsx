99d78b14efbe8dbbf1521c68439f21a21b4fd660
David <dfl8@aluno.ifal.edu.br>
2022-10-17 18:16:25 -0300

    feat: Finder addition

--- src/components/Finder.tsx
+++ src/components/Finder.tsx
@@ -1,3 +1,13 @@
+import { View, TextInput } from 'react-native'
+import { FontAwesome } from "@expo/vector-icons";
+
 export default function Finder() {
-    
+    return (
+        <View className="m-4 rounded p-3 border-blue-700 border flex-row justify-between">
+            <TextInput
+                placeholder="Search a Task"
+            ></TextInput>
+            <FontAwesome name="search" size={24} color="#1d4ed8" />
+        </View>
+    );
 }
--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,9 +1,11 @@
+import Finder from "../components/Finder";
 import { HeaderHome } from "../components/Headers/HeaderHome";
 
 export function Home() {
   return (
     <>
         <HeaderHome title='MyTasks'/>
+        <Finder/>
     </>
   );
 }
