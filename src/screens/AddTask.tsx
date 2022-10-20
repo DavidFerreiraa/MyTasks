78df608362477fb680bc529832ad51b42c4182b9
David <dfl8@aluno.ifal.edu.br>
2022-10-19 22:51:17 -0300

    feat: Save task btn addition

--- src/screens/AddTask.tsx
+++ src/screens/AddTask.tsx
@@ -1,6 +1,6 @@
 import { useNavigation } from "@react-navigation/native";
 import { useState } from "react";
-import { SafeAreaView } from "react-native";
+import { Image, SafeAreaView, ScrollView } from "react-native";
 import Field from "../components/Field";
 import { TitleAndDesc } from "../components/TitleAndDesc";
 import DateTimePicker from "@react-native-community/datetimepicker";
@@ -11,55 +11,77 @@ export function AddTask() {
     const navigator = useNavigation()
 
     const [timePicker, showTimePicker] = useState<boolean>(false)
+    const [sendBtn, showSendBtn] = useState<boolean>(false);
     const [title, setTitle] = useState<string>("")
     const [desc, setDesc] = useState<string>("")
+    var fTime: string = ""
     var [date, setDate] = useState<Date>(new Date())
 
     return (
-        <SafeAreaView className="w-full h-full bg-blue-700 p-8">
-            <TitleAndDesc
-                Title="MyTasks"
-                Description="Add a new task below"
-                textColor="text-white"
-                className="mb-20"
-            />
-            <Field
-                title="Add task title"
-                getValue={setTitle}
-                className="bg-white"
-                textColor="text-white"
-            />
-            <Field
-                title="Add task description"
-                getValue={setDesc}
-                className="bg-white h-32"
-                textColor="text-white"
-                multiline={true}
-            />
-            <Btn
-                btntext="Set initial time"
-                className="w-full bg-blue-300 p-4"
-                onPress={() => {
-                    showTimePicker(!timePicker);
-                }}
-            />
-            {timePicker && (
-                <DateTimePicker
-                    testID="dateTimePicker"
-                    value={date}
-                    display="default"
-                    mode="time"
-                    is24Hour={true}
-                    onChange={async (event, dateChanged) => {if (dateChanged) {
-                        await setDate(dateChanged)
-                        let tempDate = dateChanged
-                        let fTime = `${tempDate.getHours()}:${tempDate.getMinutes()}`
-                        showTimePicker(!timePicker)
-                    }
-                console.log(date)
-                }}
+        <SafeAreaView className="w-full h-full bg-blue-700">
+            <ScrollView className="w-full h-full p-8">
+                <TitleAndDesc
+                    Title="MyTasks"
+                    Description="Add a new task below"
+                    textColor="text-white"
+                    className="mb-20"
                 />
-            )}
+                <Field
+                    title="Add task title"
+                    getValue={setTitle}
+                    className="bg-white"
+                    textColor="text-white"
+                />
+                <Field
+                    title="Add task description"
+                    getValue={setDesc}
+                    className="bg-white h-32"
+                    textColor="text-white"
+                    multiline={true}
+                />
+                <Btn
+                    btntext="Set initial time"
+                    className="w-full bg-blue-300 p-4 rounded mt-8 items-center"
+                    onPress={() => {
+                        showTimePicker(!timePicker);
+                    }}
+                />
+                {timePicker && (
+                    <DateTimePicker
+                        testID="dateTimePicker"
+                        value={date}
+                        display="default"
+                        mode="time"
+                        is24Hour={true}
+                        onChange={async (event, dateChanged) => {
+                            if (dateChanged) {
+                                await setDate(dateChanged);
+                                let tempDate = dateChanged;
+                                fTime = `${String(tempDate.getHours()).padStart(
+                                    2,
+                                    "0"
+                                )}:${String(tempDate.getMinutes()).padStart(
+                                    2,
+                                    "0"
+                                )}`;
+                                showTimePicker(!timePicker);
+                                showSendBtn(!sendBtn);
+                            }
+                            console.log(fTime);
+                        }}
+                    />
+                )}
+                {sendBtn ? (
+                    <Btn
+                        btntext="Save task"
+                        className="w-full bg-blue-300 p-4 rounded mt-8 items-center"
+                    />
+                ) : null}
+                <Image
+                    source={require("../assets/Add_Next_task.png")}
+                    className="w-60 h-60 mt-12 mx-auto"
+                />
+            </ScrollView>
         </SafeAreaView>
     );
 }
