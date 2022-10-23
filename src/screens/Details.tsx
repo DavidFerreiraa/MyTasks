98b3c6d7c694e1225ddcdd034bcc3e93869c791a
David <dfl8@aluno.ifal.edu.br>
2022-10-23 01:42:23 -0300

    feat: Sucess btn and edit btn funcionalities added

--- src/screens/Details.tsx
+++ src/screens/Details.tsx
@@ -2,12 +2,28 @@ import { useNavigation } from "@react-navigation/native";
 import { SafeAreaView, ScrollView, Image, Text, View, TouchableOpacity } from "react-native";
 import { TitleAndDesc } from "../components/TitleAndDesc";
 import { AntDesign, Entypo } from "@expo/vector-icons";
+import api from "../services/api";
 
 export function Details(props: any) {
 
     const navigator = useNavigation()
     const TasksData = props.route.params.data;
     console.log(TasksData)
+
+    const deleteTask = async (id: string) => {
+        await api
+            .delete(`/task/${id}`)
+            .then((response) => navigator.navigate("home"))
+            .catch((err) => {
+            });
+    };
+
+    const editTask = async (id: string) => {
+        console.log("Entrou");
+        navigator.navigate("editionscreen", {
+            id: id,
+        });
+    };
 
     return (
         <SafeAreaView className="bg-blue-700 w-full h-full">
@@ -32,12 +48,12 @@ export function Details(props: any) {
                 />
                 <View className="flex-row justify-center gap-12 mt-8">
                     <TouchableOpacity onPress={() => {
-
+                        deleteTask(TasksData.id)
                     }}>
                         <AntDesign name="checkcircle" size={60} color="white" />
                     </TouchableOpacity>
                     <TouchableOpacity onPress={() => {
-                        
+                        editTask(TasksData.id)
                     }}>
                         <AntDesign name="form" size={60} color="white" />
                     </TouchableOpacity>
